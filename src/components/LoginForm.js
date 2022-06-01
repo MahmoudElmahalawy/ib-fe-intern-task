1a4f5cf046c08801cb3103d5be5ae1e6d12c26ef
MahmoudElmahalawy <mahmoud.hamdy.elmahalawy@gmail.com>
2022-06-01 14:54:22 +0200

    Authentication complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
 	const [buttonDisabled, setButtonDisabled] = useState();
+	const [incorrectCreds, setIncorrectCreds] = useState(false);
+
+	const navigate = useNavigate();
 
 	const {
 		register,
@@ -11,7 +16,7 @@ const LoginForm = () => {
 		getValues,
 		watch,
 		formState: { errors },
-	} = useForm();
+	} = useForm({ defaultValues: { email: "", password: "" } });
 
 	const watchAllFields = watch();
 
@@ -25,7 +30,33 @@ const LoginForm = () => {
 		if (!password1num1upcRegex.test(value)) return "Password must contain 1 or more uppercase letters and numbers.";
 	};
 
-	const onSubmit = (data) => console.log(data, errors);
+	const authenticateUser = (data) => {
+		const validUsers = [
+			{ email: "mohamed@instabug.com", password: "A12345678" },
+			{ email: "mohamed1@instabug.com", password: "A12345678" },
+			{ email: "mohamed2@instabug.com", password: "A12345678" },
+			{ email: "mohamed3@instabug.com", password: "A12345678" },
+			{ email: "mohamed4@instabug.com", password: "A12345678" },
+			{ email: "mohamed5@instabug.com", password: "A12345678" },
+			{ email: "mohamed6@instabug.com", password: "A12345678" },
+			{ email: "mohamed7@instabug.com", password: "A12345678" },
+		];
+
+		const enteredEmail = validUsers.find((user) => user.email === data.email);
+
+		if (!enteredEmail || enteredEmail.password !== data.password) {
+			setIncorrectCreds(true);
+		} else {
+			setIncorrectCreds(false);
+			localStorage.setItem(
+				"loggedUser",
+				JSON.stringify({ username: data.email.split("@")[0], email: data.email })
+			);
+			navigate("/welcome");
+		}
+	};
+
+	const onSubmit = (data) => authenticateUser(data);
 
 	useEffect(() => {
 		if (!(getValues().email && getValues().password)) {
@@ -37,6 +68,7 @@ const LoginForm = () => {
 
 	return (
 		<form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+			{incorrectCreds && <div className="incorrect-creds">Your email and/or password are incorrect</div>}
 			<div className="form-control">
 				<label htmlFor="email">Work Email</label>
 				<input
